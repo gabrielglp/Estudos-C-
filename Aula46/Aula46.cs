6b9d889419b2e9f6abe7c2293332d49f109cbfe0
Gabriel de Souza Martins <83505259+gabrielglp@users.noreply.github.com>
2022-04-11 13:26:24 -0300

    Estudo c#

--- Aula46/Aula46.cs
+++ Aula46/Aula46.cs
@@ -1,8 +1,45 @@
 using System;
 
+class Galinha
+{
+    private string nomeGalinha;
+
+    private int numOvo;
+
+    public Galinha(string nomeGalinha)
+    {
+        this.nomeGalinha = nomeGalinha;
+        numOvo = 0;
+    }
+
+    public Ovo botar()
+    {
+        numOvo++;
+        return new Ovo(numOvo, nomeGalinha);
+    }
+}
+
+class Ovo
+{
+    private int numOvo;
+
+    private string minhaGalinha;
+
+    public Ovo(int numOvo, string minhaGalinha)
+    {
+        this.numOvo = numOvo;
+        this.minhaGalinha = minhaGalinha;
+        Console.WriteLine("Ovo criado: {0} - {1}", numOvo, minhaGalinha);
+    }
+}
+
 class Aula46
 {
     static void Main()
     {
+        Galinha g1 = new Galinha("JOSÉFA");
+        Galinha g2 = new Galinha("FERNANDA");
+
+        g1.botar();
     }
 }
